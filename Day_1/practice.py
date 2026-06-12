50f265c92636bdebc2892b5bf0d902964d0faa18
Rohith9052268006 <153802035+Rohith9052268006@users.noreply.github.com>
2026-06-12 07:02:27 +0000

    Deleted

--- Day_1/practice.py
+++
@@ -1,8 +0,0 @@
-def pass_fail():
-    name = input("Enter your name")
-    marks=int(input("Enter your marks"))
-    if(marks>35):
-        print(name, "is pass")
-    else:
-        print(name,"is fail")
-pass_fail()
